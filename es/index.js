7408e44f4d07001fa9ed8ef92da4b3d53983fd2f
fengy <fengy@bitfid.com>
2017-12-05 17:53:13 +0800

    taurus npm run  build feedprice 1

--- es/index.js
+++ es/index.js
@@ -41,4 +41,4 @@ var FetchChainObjects = ChainStore.FetchChainObjects,
     FetchChain = ChainStore.FetchChain;
 
 
-export { ChainStore, TransactionBuilder, FetchChainObjects, ChainTypes, EmitterInstance, ObjectId, NumberUtils, TransactionHelper, ChainValidation, FetchChain, Login, GetSubjectById, GetSubjectsByName, GetSubjectsOrderById, GetSubjectsByStatus, GetSubjectsByCreator, GetSubjectsByCreateTime, GetSubjectsByVoteEndTime, GetSubjectVotesByVoter, GetSubjectVotesBySubjectId, FrontPageGetSubjects, MarketGetSubjects, MyGetSubjects, GetMyCreateSubjects, GetFeedPrice };
+export { ChainStore, TransactionBuilder, FetchChainObjects, ChainTypes, EmitterInstance, ObjectId, NumberUtils, TransactionHelper, ChainValidation, FetchChain, Login, GetSubjectById, GetSubjectsByName, GetSubjectsOrderById, GetSubjectsByStatus, GetSubjectsByCreator, GetSubjectsByCreateTime, GetSubjectsByVoteEndTime, GetSubjectVotesByVoter, GetSubjectVotesBySubjectId, FrontPageGetSubjects, MarketGetSubjects, MyGetSubjects, GetMyCreateSubjects, GetFeedPrice, GetLatestValidPrice, GetLatestValidPriceBatch };
